c1de3b68562a545997e109405732b5deb1bcf394
dethsanius <dethsanius@gmail.com>
2017-07-27 18:10:51 +0200

    redused for loops kept funcionality

--- Server/Collector/Clan.cs
+++ Server/Collector/Clan.cs
@@ -7,6 +7,7 @@ namespace Collector {
         public List<User> users {get;}
         public string name {get;}
         public long xp {get; private set;}
+        private List<string> usernames = new List<string>();
         public Clan(string name) {
             this.name = name;
             this.users = new List<User>();
@@ -15,15 +16,13 @@ namespace Collector {
             xp = 0;
             string ClanUsers = Web.MakeAsyncRequest("http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=" + name, "text/csv");
             string[] items = ClanUsers.Split(new string[]{",", "\r", "\n", "\r\n", Environment.NewLine}, System.StringSplitOptions.RemoveEmptyEntries);
-            var usernames = new List<string>();
-            foreach (User user in users) {
-                usernames.Add(user.name);
-            }
             for (int i = 4; i < items.Length; i++) {
                 if (i % 4 == 0) {
                     string username = items[i].Replace("?", " ");
-                    if (!usernames.Contains(username))
+                    if (!usernames.Contains(username)) {
                         this.users.Add(new User(username, name));
+                        usernames.Add(username);
+                    }
                 }
             }
             foreach (User user in users) {
